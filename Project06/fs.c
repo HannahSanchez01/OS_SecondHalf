aa345d59addb08a94dcb2f5773156689763b5a12
corcoranNoah <noahcorcoran0308@gmail.com>
2023-04-28 11:16:04 -0400

    attempt fs mount

--- Project06/fs.c
+++ Project06/fs.c
@@ -8,8 +8,16 @@ Make your changes here.
 
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
 
 extern struct disk *thedisk;
+
+//Noah - bitmap array - malloc in mount - value is 1 if used - 0 if not
+int* bitmap;
+
+//Noah - Can only mount once - value changes to 1 after successful mount
+int isMounted = 0;
 
 int fs_format()
 {
@@ -74,7 +82,72 @@ void fs_debug()
 
 int fs_mount()
 {
-	return 0;
+	union fs_block block;
+
+	disk_read(thedisk,0,block.data);
+
+	//Noah - check if a disk already mounted
+	if(isMounted)
+	{
+		printf("Mount: already mounted disk\n");
+		return 0;
+	}
+
+	if (block.super.magic != FS_MAGIC)
+	{
+		printf("Mount: invalid magic number\n");
+		return 0;
+	}
+
+	int size = disk_size(thedisk);
+	//Noah- Check if disk size and number of blocks match
+	if(size != block.super.nblocks)
+	{
+		printf("Mount: Number of blocks and disk size do not match\n");
+		return 0;
+	}
+
+	//Noah - Allocate space based on # blocks
+	bitmap = (int*)malloc(block.super.nblocks * sizeof(int));
+	memset(bitmap,0,block.super.nblocks);
+
+	for (int i=1; i< block.super.ninodeblocks+1; i++)//Loop through each inode block
+	{
+		disk_read(thedisk, i, block.data);
+		for (int j=0; j<INODES_PER_BLOCK; j++){//Loop through inodes in each block
+			if (block.inode[j].isvalid)//Found valid inode - find which blocks (direct or indirect) are used by inode
+			{
+				for (int k=0; k< POINTERS_PER_INODE; k++)//Loop through direct pointers
+				{
+					if (block.inode[j].direct[k]){
+						bitmap[(int)block.inode[j].direct[k]] = 1;//block is in use by direct pointer
+					}
+				}
+
+				if (block.inode[j].indirect)//There is indirection
+				{
+					bitmap[(int)block.inode[j].indirect] = 1;//Indirect block in use
+					disk_read(thedisk, block.inode[j].indirect, block.data);
+					for (int k=0; k<POINTERS_PER_BLOCK; k++){// find indirect blocks
+						if (block.pointers[k]){
+							bitmap[block.pointers[k]] = 1;//Found indirect blocks
+						}
+					}
+					disk_read(thedisk, i, block.data);
+				}
+			}
+		}
+	}
+	for(int i = 0; i<block.super.nblocks; i++)
+	{
+		if(bitmap[i])
+		{
+			printf("%d ",i);
+		}
+	}
+	printf("\n");
+	isMounted = 1;//Noah - Successful mount - can't do another mount
+	return 1;
 }
 
 int fs_create()
